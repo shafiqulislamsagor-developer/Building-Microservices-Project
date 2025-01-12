cb616ccae402d3a87fb9d0cffd23e786de735cd6
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-01-12 21:27:52 +0600

    inventory create

---
+++ services/inventory/src/controllers/createInventory.ts
@@ -0,0 +1,41 @@
+import prisma from "@/prisma";
+import { InventoryCreateDTOSchema } from "@/schemas";
+import { NextFunction, Request, Response } from "express";
+
+const createInventory = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // Validate the request body
+    const parsedBody = InventoryCreateDTOSchema.safeParse(req.body);
+    if (!parsedBody.success) {
+      return res.status(400).json({ error: parsedBody.error.errors });
+    }
+
+    // Create inventory
+    const inventory = await prisma.inventory.create({
+      data: {
+        ...parsedBody.data,
+        histories: {
+          create: {
+            actionType: "IN",
+            quantityChanged: parsedBody.data.quantity,
+            lastQuantity: 0,
+            newQuantity: parsedBody.data.quantity,
+          },
+        },
+      },
+      select: {
+        id: true,
+        quantity: true,
+      },
+    });
+    return res.status(200).json(inventory);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default createInventory;
--- services/inventory/src/index.ts
+++ services/inventory/src/index.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
+import { createInventory } from "./controllers";
 
 dotenv.config();
 
@@ -15,16 +16,28 @@ app.get("/health", (_req, res) => {
   res.status(200).json({ status: "UP" });
 });
 
-const port = process.env.PORT || 4002;
-const serviceName = process.env.SERVICE_NAME || "inventory-service";
+// routes
+app.post("/inventories", createInventory);
 
 // 404 handler
+
+app.use((_req, res) => {
+  res.status(404).json({
+    message: "Not Found",
+  });
+});
+
+// Error handler
 app.use((err, _req, res, _next) => {
   console.log(err.stack);
   res.status(404).json({
     message: "Internal Server Error",
   });
 });
+
+const port = process.env.PORT || 4002;
+const serviceName = process.env.SERVICE_NAME || "inventory-service";
+
 app.listen(port, () => {
   console.log(`${serviceName} is running on port ${port}`);
 });
